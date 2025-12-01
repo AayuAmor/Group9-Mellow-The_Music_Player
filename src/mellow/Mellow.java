3a158e142d5d7af506aafa18c8c81cf5136acfd7
AayuAmor <aayuamor@gmail.com>
2025-12-01 20:58:36 +0545

    First Commit

---
+++ src/mellow/Mellow.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mellow;
+
+/**
+ *
+ * @author oakin
+ */
+public class Mellow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
